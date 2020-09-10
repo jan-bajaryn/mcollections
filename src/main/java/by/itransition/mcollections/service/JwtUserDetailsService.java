eca260dc3d5e4746c7ce004f45c9a29bc845c464
Jan <jabajaryn@gmail.com>
2020-09-10 16:47:17 +0300

    some comments deleted

--- src/main/java/by/itransition/mcollections/service/JwtUserDetailsService.java
+++ src/main/java/by/itransition/mcollections/service/JwtUserDetailsService.java
@@ -2,13 +2,10 @@ package by.itransition.mcollections.service;
 
 import by.itransition.mcollections.repos.UserRepo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
